5a7391a318411b9fc90800214e5b46eac4041d23
seyedmahdi79 <sidabas.1363@gmail.com>
2021-12-07 13:46:32 +0330

    .

--- 1.py
+++
@@ -1,3 +0,0 @@
-number=int(input())
-list=[i for i in range(2,number) if i%3==0 or i%5==0]    
-print(sum(list))
--- 3.py
+++
@@ -1,8 +0,0 @@
-
-n=int(input())
-i=2
-while i<n:
-    while n%i==0:
-       n=n/i
-    i=i+1
-print(n)
